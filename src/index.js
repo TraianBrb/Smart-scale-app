885f262c906d2c581737648380398eb1c25e67af
Traian Barbu <traian.barbu@iquestgroup.com>
2018-09-17 16:29:37 +0300

    First commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import style from './style.scss';
+
+const Index = () => {
+  return <div>Hello!</div>;
+};
+
+ReactDOM.render(<Index />, document.getElementById('index'));
